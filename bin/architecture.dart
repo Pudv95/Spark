36196a18904f1183904d9a50293b7c69f38d4193
Pudv95 <vivekupadhayay18@gmail.com>
2024-04-28 22:45:09 +0530

    improve the commands and options

--- bin/architecture.dart
+++ bin/architecture.dart
@@ -7,59 +7,66 @@ import 'package:Spark/add_feature.dart';
 
 /// The main entry point of the application.
 void main(List<String> arguments) {
-  // Initialize the ArgParser
+  // Initialize argument parser
   final argParser = ArgParser();
 
   // Add commands
+  argParser.addCommand('init', argParser);
   argParser.addCommand('create', argParser);
-  argParser.addCommand('getAssets', argParser);
-  argParser.addCommand('init_routes', argParser);
-  argParser.addCommand('pubGet', argParser);
-
-  // Add options
-  argParser.addOption('addFeature', abbr: 'f', help: 'Add a new feature');
-  argParser.addOption('removeFeature',
+  argParser.addOption('add_feature', abbr: 'a', help: 'Add a new feature');
+  argParser.addOption('remove_feature',
       abbr: 'r', help: 'Removes an existing feature');
-  argParser.addOption('create', abbr: 'c', help: 'Add a new feature');
 
-  // Parse arguments
+  // Parse command line arguments
   final argResults = argParser.parse(arguments);
 
-  // Initialize objects
+  // Print parsed command, rest arguments, and options
+  print(argResults.command?.name);
+  print(argResults.command?.rest);
+  print(argResults.command?.options);
+
+  // Initialize necessary classes
   final FolderGenerator folderGenerator = FolderGenerator();
   final AssetManagement assetManagement = AssetManagement();
   final RoutingManagement routingManagement = RoutingManagement();
   final CreateApp createApp = CreateApp();
 
-  // Check which command or option was provided and take appropriate actions
+  // Execute commands based on parsed arguments
   if (argResults.command?.name == 'create') {
-    // If 'create' command was provided
-    folderGenerator.generate(); // Generate folders
-  } else if (argResults.command?.name == 'getAssets') {
-    // If 'getAssets' command was provided
-    assetManagement.getAllAssets(); // Retrieve all assets
-  } else if (argResults.command?.name == 'init_routes') {
-    // If 'init_routes' command was provided
-    routingManagement.initialiseRouting(); // Initialize routing
-  } else if (argResults.command?.name == 'pubGet') {
-    // If 'init_routes' command was provided
-    createApp.updatePubspecYaml(); // Initialize routing
-  } else if (argResults['addFeature'] != null) {
-    // If 'addFeature' option was provided
-    final featureName = argResults['addFeature'] as String;
+    String? appName = argResults.command?.rest[0];
+    if (appName == null) {
+      print('Please provide a name for the app');
+      return;
+    } else {
+      createApp.createApp(appName);
+    }
+  } else if (argResults.command?.name == 'init') {
+    if (argResults.command?.rest[0] == 'folders') {
+      // Initialize folder structure
+      folderGenerator.generate();
+    } else if (argResults.command?.rest[0] == 'assets') {
+      // Manage assets
+      assetManagement.getAllAssets();
+    } else if (argResults.command?.rest[0] == 'routes') {
+      // Initialize routing
+      routingManagement.initialiseRouting();
+    } else {
+      // Command not recognized
+      print('This command is not recognized. Please try again.');
+    }
+  }
+
+  // Add feature if specified
+  if (argResults['add_feature'] != null) {
+    final featureName = argResults['add_feature'] as String;
     final Features features = Features(featureName: featureName);
-    features.generateFeature(); // Generate the specified feature
-  } else if (argResults['removeFeature'] != null) {
-    // If 'removeFeature' option was provided
-    final featureName = argResults['removeFeature'] as String;
+    features.generateFeature();
+  }
+
+  // Remove feature if specified
+  if (argResults['remove_feature'] != null) {
+    final featureName = argResults['remove_feature'] as String;
     final Features features = Features(featureName: featureName);
-    features.removeFeature(); // Remove the specified feature
-  } else if (argResults['create'] != null) {
-    // If 'init_routes' command was provided
-    final appName = argResults['create'] as String;
-    createApp.createApp(appName); // Initialize routing
-  } else {
-    // If none of the expected commands or options were provided
-    print('This command is not recognized. Please try again.');
+    features.removeFeature();
   }
 }
